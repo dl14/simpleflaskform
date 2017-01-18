28268251c3198fc2e64e2c2ad97860683ef11d01
dl14 <dev.lakhia14@gmail.com>
2017-01-18 13:17:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request
+
+import glexi
+
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/mainpage', methods=['POST', 'GET'])
 def hello_world():
     searchword = request.form.get('searchword')
+    print "calling glexi"
+    glexi.scrape_definition(searchword)
+    print "called glexi"
     return render_template('main.html', searchword=searchword)
 
 if __name__ == "__main__":
